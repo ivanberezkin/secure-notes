9f002e91676dbd941813e65533f620bae57b47b3
ivbe0111 <berezkinivan94@gmail.com>
2026-04-14 01:22:03 +0200

    imported tailwindcss

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,7 @@
 function App() {
-  return <p className="App">Hello, World!</p>;
+  return (
+    <h1 className="text-3xl font-bold text-blue-500">Hello, Tailwind CSS!</h1>
+  );
 }
 
 export default App;
