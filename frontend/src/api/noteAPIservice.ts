e79267bec888e1829ccf6e270ea4cfbb527f1707
ivbe0111 <berezkinivan94@gmail.com>
2026-04-14 21:35:45 +0200

    implemented suggestions from coderabbit

--- frontend/src/api/noteAPIservice.ts
+++ frontend/src/api/noteAPIservice.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import type { Note } from "../types/note";
 
-const API_BASE_URL = "http://localhost:8080/api/notes";
+const API_BASE_URL = `${import.meta.env.VITE_API_BASE_URL}/api/notes`;
 
 export const getNotes = async () => {
   try {
