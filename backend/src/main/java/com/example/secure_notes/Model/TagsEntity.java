745632308ffb2f0a4c9c5af80852cbb67a5770e4
ivbe0111 <berezkinivan94@gmail.com>
2026-04-15 13:01:30 +0200

    added implements coderabbit suggested

--- backend/src/main/java/com/example/secure_notes/Model/TagsEntity.java
+++ backend/src/main/java/com/example/secure_notes/Model/TagsEntity.java
@@ -1,13 +1,14 @@
 package com.example.secure_notes.Model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@Table(name = "notes")
+@Table(name = "tags")
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
@@ -19,6 +20,9 @@ public class TagsEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull
+    private String tagName;
 
     @ManyToOne
     @JoinColumn(name = "note_id")
--- frontend/src/api/userAPIservice.ts
+++ frontend/src/api/userAPIservice.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "${import.meta.env.VITE_API_BASE_URL}/api/users";
+const API_BASE_URL = `${import.meta.env.VITE_API_BASE_URL}/api/users`;
 
 export const getUser = async (userId: number) => {
   try {
