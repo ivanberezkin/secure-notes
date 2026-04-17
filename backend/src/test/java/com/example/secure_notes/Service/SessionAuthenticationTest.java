89ccd69448380ef6b40465c067fa7d0a5e7c2ce0
ivbe0111 <berezkinivan94@gmail.com>
2026-04-17 21:14:52 +0200

    went back to working tests, ignoring testing of JSESSIONID. Broke out login user to own method

--- backend/src/test/java/com/example/secure_notes/Service/SessionAuthenticationTest.java
+++ backend/src/test/java/com/example/secure_notes/Service/SessionAuthenticationTest.java
@@ -49,29 +49,7 @@ public class SessionAuthenticationTest {
     public void authenticatedRequest_shouldSucceed_withSessionCookie() throws Exception {
 
         MockHttpSession session = new MockHttpSession();
-
-        MvcResult csrfResult = mockMvc.perform(post("/login")
-                        .session(session))
-                .andReturn();
-
-        Cookie csrfCookie = csrfResult.getResponse().getCookie("XSRF-TOKEN");
-        assertNotNull (csrfCookie, "XSRF-TOKEN present");
-        String csrfToken = csrfCookie.getValue();
-
-        MvcResult loginResult = mockMvc.perform(post("/login")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .session(session)
-                        .cookie(csrfCookie)
-                        .header("X-XSRF-TOKEN", csrfToken)
-                        .content("""
-                                {
-                                    "username": "testuser",
-                                    "password": "12345678"
-                                }
-                                """))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andReturn();
+        loginUser(session);
 
         mockMvc.perform(get("/api/users")
                         .session(session))
@@ -85,18 +63,32 @@ public class SessionAuthenticationTest {
     }
 
     @Test
-    public void authenticatedRequestLogout_TryingToAccessApiUsers_shouldReturn403() throws Exception{
+    public void authenticatedRequestLogout_TryingToAccessApiUsers_shouldReturn403() throws Exception {
         MockHttpSession session = new MockHttpSession();
+        Cookie csrfCookie = loginUser(session);
 
+        mockMvc.perform(post("/logout")
+                .session(session)
+                .cookie(csrfCookie)
+                .header("X-XSRF-TOKEN", csrfCookie.getValue()));
+        assertTrue(session.isInvalid());
+
+        mockMvc.perform(get("/api/users")
+                        .session(session))
+                .andExpect(status().isForbidden()).andReturn();
+
+    }
+
+    private Cookie loginUser(MockHttpSession session) throws Exception {
         MvcResult csrfResult = mockMvc.perform(post("/login")
                         .session(session))
                 .andReturn();
 
         Cookie csrfCookie = csrfResult.getResponse().getCookie("XSRF-TOKEN");
-        assertNotNull (csrfCookie, "XSRF-TOKEN present");
+        assertNotNull(csrfCookie, "XSRF-TOKEN present");
         String csrfToken = csrfCookie.getValue();
 
-        MvcResult loginResult = mockMvc.perform(post("/login")
+        mockMvc.perform(post("/login")
                         .contentType(MediaType.APPLICATION_JSON)
                         .session(session)
                         .cookie(csrfCookie)
@@ -107,21 +99,10 @@ public class SessionAuthenticationTest {
                                     "password": "12345678"
                                 }
                                 """))
-                .andDo(print())
                 .andExpect(status().isOk())
                 .andReturn();
 
-        mockMvc.perform(post("/logout")
-                .session(session)
-                .cookie(csrfCookie)
-                .header("X-XSRF-TOKEN", csrfToken));
-
-        assertTrue(session.isInvalid());
-
-        mockMvc.perform(get("/api/users")
-                .session(session))
-                .andExpect(status().isForbidden()).andReturn();
-
+        return csrfCookie;
     }
 
 }
