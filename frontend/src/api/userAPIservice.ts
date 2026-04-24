3a13ed2cc723b493f8cbfff3340b1deafad36a37
ivbe0111 <berezkinivan94@gmail.com>
2026-04-24 14:14:56 +0200

    added loginUser and getCurrentUser to api

--- frontend/src/api/userAPIservice.ts
+++ frontend/src/api/userAPIservice.ts
@@ -2,12 +2,31 @@ import axios from "axios";
 
 const API_BASE_URL = `${import.meta.env.VITE_API_BASE_URL}/api/users`;
 
-export const getUser = async (userId: number) => {
+export const getCurrentUser = async () => {
   try {
-    const response = await axios.get(`${API_BASE_URL}/${userId}`);
+    const response = await axios.get(`${API_BASE_URL}/me`, {
+      withCredentials: true,
+    });
     return response.data;
   } catch (error) {
-    console.error("Error fetching user:", error);
+    console.error("Error fetching current user:", error);
+    throw error;
+  }
+};
+
+export const loginUser = async (username: string, password: string) => {
+  try {
+    const response = await axios.post(
+      `${API_BASE_URL}/login`,
+      {
+        username,
+        password,
+      },
+      { withCredentials: true },
+    );
+    return response.data;
+  } catch (error) {
+    console.error("Error logging in:", error);
     throw error;
   }
 };
